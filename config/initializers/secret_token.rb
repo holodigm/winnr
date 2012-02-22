69f3b002e3d1a33fe5c8abce69a8827de8ef115c
kym <kym@kym-laptop-ubuntu.(none)>
2012-02-22 14:26:25 +1100

    initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Winnr::Application.config.secret_token = 'b7a5d1255a9d469b56d007c2ef5b0c8b0fa3ea0dde337a33eb43bbe61b8225cc85cee1634a58c0fc7a2ba65563f5121c25a068f356b49ddb597b3e14f4b2fa98'
